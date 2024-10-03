93bf0b73424bc19b66870ff9f74ff5448eb0a35b
Ryan Bauroth <25bauroth@da.org>
2024-10-03 19:57:23 -0400

    added kill and recent move board colors

--- game.py
+++ game.py
@@ -16,6 +16,8 @@ BACKGROUND_COLOR = (247,247,247)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 LIGHT_GRAY = (180, 180, 180)
+LATEST_MOVE_COLOR = (206, 223, 159)
+KILL_COLOR = (245, 232, 175)
 KING_COLOR = (255, 255, 0)
 CORNER_COLOR = (233, 196, 177)
 PIECE_COLORS = [(0, 0, 0), BLACK, WHITE, KING_COLOR]
@@ -48,6 +50,7 @@ pygame.display.set_caption("Hnefatafl")
 
 # Game Vars
 board = [[0 for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+recent_move_cords = {}
 piece_arr = []
 turn = 1
 
@@ -96,7 +99,6 @@ def append_draw_piece(row, col, piece):
 
 # Draw the board
 def draw_board():
-    screen.fill(BACKGROUND_COLOR)
     pygame.draw.rect(screen, LIGHT_GRAY, (0, 0, WINDOW_SIZE, WINDOW_SIZE), 2)
     for corner in CORNERS:
         row, col = corner
@@ -124,43 +126,58 @@ Checks for kills
 :param piece: int the most recently moved piece's piece id
 """
 def check_kills(row, col, piece):
+    kill_cords = []
     enemy_piece = 1 if piece == 2 else 2
+
     # north direction check
     if row - 2 >= 0:
         # normal piece capture
         if board[row - 1][col] == enemy_piece and (board[row - 2][col] == piece or (row - 2, col) in CORNERS):
             board[row - 1][col] = 0
+            kill_cords.append((row - 1, col))
+
     # north king capture
     if col - 1 > 0 and col + 1 < BOARD_SIZE - 1 and row != BOARD_SIZE - 1 and row != 0:
         if piece == 1 and board[row - 1][col] == 3 and (row - 2 < 0 or board[row - 2][col] == piece) and board[row - 1][col - 1] == piece and board[row - 1][col + 1] == piece:
             print("ATTACKERS WIN")
+
     # south direction check
     if row + 2 <= BOARD_SIZE - 1:
         if board[row + 1][col] == enemy_piece and (board[row + 2][col] == piece or (row + 2, col) in CORNERS):
             board[row + 1][col] = 0
+            kill_cords.append((row + 1, col))
+
     # south king capture
     if col - 1 > 0 and col + 1 < BOARD_SIZE - 1 and row != BOARD_SIZE - 1 and row != 0:
         if piece == 1 and board[row + 1][col] == 3 and (row + 2 < BOARD_SIZE or board[row + 2][col] == piece) and \
                 board[row + 1][col - 1] == piece and board[row + 1][col + 1] == piece:
             print("ATTACKERS WIN")
+
     # west direction check
     if col - 2 >= 0:
         if board[row][col - 1] == enemy_piece and (board[row][col - 2] == piece or (row, col - 2) in CORNERS):
             board[row][col - 1] = 0
+            kill_cords.append((row, col - 1))
+
     # west king check
     if row - 1 > 0 and row + 1 < BOARD_SIZE - 1 and col != BOARD_SIZE - 1 and col != 0:
         if piece == 1 and board[row][col - 1] == 3 and (col - 2 < 0 or board[row][col - 2] == piece) and \
                 board[row - 1][col - 1] == piece and board[row + 1][col - 1] == piece:
             print("ATTACKERS WIN")
+
     # east direction check
     if col + 2 <= BOARD_SIZE - 1:
         if board[row][col + 1] == enemy_piece and (board[row][col + 2] == piece or (row, col + 2) in CORNERS):
             board[row][col + 1] = 0
+            kill_cords.append((row, col + 1))
+
     # east king check
     if row - 1 > 0 and row + 1 < BOARD_SIZE - 1 and col != BOARD_SIZE - 1 and col != 0:
         if piece == 1 and board[row][col + 1] == 3 and (col + 2 > BOARD_SIZE - 1 or board[row][col + 2] == piece) and \
                 board[row - 1][col + 1] == piece and board[row + 1][col + 1] == piece:
             print("ATTACKERS WIN")
+
+    return kill_cords
 
 """
 Checks to see if an intended play is legal
@@ -207,6 +224,20 @@ def check_play(grid_y, row, grid_x, col, piece):
                     return False
     return True
 
+def update_field_colors(col, row, color):
+    pygame.draw.rect(screen, color, (col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE), 0)
+
+def clear_field_colors():
+    global recent_move_cords
+    recent_move_cords = {
+        "original_col": -1,
+        "original_row": -1,
+        "new_col": -1,
+        "new_row": -1,
+    }
+
+kill_cords = []
+
 """   
 Game loop
 
@@ -216,14 +247,18 @@ clock = pygame.time.Clock()
 def main():
     setup_board()
     draw_board()
+    clear_field_colors()
 
     global piece_arr
     global turn
+    global recent_move_cords
+    global kill_cords
 
     is_dragging = False
     selected_piece = None
     while True:
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
@@ -254,7 +289,13 @@ def main():
                         board[grid_y][grid_x] = piece
                         # if the piece is not going back to its og location, change turns
                         if not (grid_y == row and grid_x == col):
-                            check_kills(grid_y, grid_x, piece)
+                            kill_cords = check_kills(grid_y, grid_x, piece)
+                            recent_move_cords = {
+                                "original_col": col,
+                                "original_row": row,
+                                "new_col": grid_x,
+                                "new_row": grid_y,
+                            }
                             turn = 1 if turn == 2 else 2
                     else:
                         # if the user is playing in a full spot, return the piece to its og location
@@ -262,6 +303,13 @@ def main():
                 is_dragging = False
 
         piece_arr = []
+        screen.fill(BACKGROUND_COLOR)
+        if recent_move_cords["original_row"] != -1:
+            update_field_colors(recent_move_cords["original_col"], recent_move_cords["original_row"], LATEST_MOVE_COLOR)
+            update_field_colors(recent_move_cords["new_col"], recent_move_cords["new_row"], LATEST_MOVE_COLOR)
+        for kill_cord in kill_cords:
+            x, y = kill_cord
+            update_field_colors(y, x, KILL_COLOR)
         draw_board()
 
         if is_dragging:
